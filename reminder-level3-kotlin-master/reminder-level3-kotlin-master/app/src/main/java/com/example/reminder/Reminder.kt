0400022db7ea50c845b358c12823dcda8c27a771
yasmine By <benyasminenl@gmail.com>
2020-05-04 17:02:03 +0200

    Imports

--- reminder-level3-kotlin-master/reminder-level3-kotlin-master/app/src/main/java/com/example/reminder/Reminder.kt
+++
@@ -1,9 +0,0 @@
-package com.example.reminder
-
-import android.os.Parcelable
-import kotlinx.android.parcel.Parcelize
-
-@Parcelize
-data class Reminder(
-    var reminderText: String
-) :Parcelable
